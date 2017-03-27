b050960886c60d41c3207858f9d3b0aa10fdfbb6
ShahuN <shaun.michael107@gmail.com>
2017-03-27 20:53:39 +0200

    Added scipy.signal.savgol_filter() to the nxn pandas.Dataframe object

---
+++ tsfresh_mini.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from scipy.signal import savgol_filter
+# https://docs.scipy.org/doc/scipy-0.18.1/reference/generated/scipy.signal.savgol_filter.html
+from matplotlib import pyplot as plt
+DF = pd.read_csv('CV_50_100.csv')
+
+# First we'll need to filter our data:
+
+
+def filter(dataframe):
+    # Convert the dataframe into an numpy.ndarray
+    # EXCLUDING the timestamp column
+    headers = list(dataframe)
+    del(headers[0])
+
+    arr = dataframe.as_matrix(headers)
+
+    # Apply filter in a loop over the Array columns
+    arr_new = savgol_filter(arr, 11, 1, axis=0)
+
+
+    return arr_new
+
+
+T = [i for i in range(len(DF))]
+
+test = filter(DF)
+
+plt.figure()
+subp_index = 330
+headers = list(DF)
+del(headers[0])
+
+for i in range(len(test[0])):
+
+    subp = subp_index + i + 1
+    plt.subplot(subp)
+    plt.title(headers[i])
+    plt.plot(T, DF[headers[i]], 'b-', label='Unfiltered')
+    plt.plot(T, test[:, i], 'r-', label='Filtered')
+
+plt.legend(loc='best')
+plt.show()
